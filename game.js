d7122cc94e707e5d582ca0da23d3cb365e6366d8
topzyray <topzyray@yahoo.com>
2023-12-06 19:59:06 +0100

    First commit

---
+++ game.js
@@ -0,0 +1,89 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+let started = false;
+
+let level = 0;
+
+// Detecting keydown event on windows document
+$(document).keydown(function() {
+    if (!started) {
+        $("#level-title").text(`Level ${level}`);
+        nextSequence();
+        started = true;
+    }
+});
+
+// 2. Detect button on click
+$(".btn").on("click", function() {
+
+    let userChosenColour = $(this).attr('id');
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour)
+    animatePress(userChosenColour)
+    checkAnswer(userClickedPattern.length - 1)
+})
+
+// 5. Function to check answers
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("Success")
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000)
+        }
+    } else {
+        console.log("Wrong")
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+// 6. Function to restart game
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+// 1. Function choose button in sequence
+function nextSequence() {
+    userClickedPattern = [];
+
+    level ++;
+
+    $("#level-title").text(`Level ${level}`);
+
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour)
+    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+// 3. Function to play sound
+function playSound(name) {
+        let sound = new Audio(`./sounds/${name}.mp3`);
+        sound.play();  
+}
+
+// 4. Function to animate on click
+function animatePress(currentColour) {
+    $(`.${currentColour}`).addClass('pressed');
+    setTimeout(function () {
+        $(`.${currentColour}`).removeClass('pressed')
+    }, 100)
+}
